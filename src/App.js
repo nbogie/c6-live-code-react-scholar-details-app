e7ead67369160602b331fe10446a26b13793c9c9
nbogie <neillbogie@googlemail.com>
2022-11-08 15:14:29 +0000

    initial naive. no map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello Cohort 6</h1>
+
+            <ScholarView name={"Aneesah"} interests={"yoga, watercolor"} />
+
+            <ScholarView name={"Alice"} interests={"video games, music"} />
+
+            <ScholarView
+                name={"Ben"}
+                interests={"breakdancing, board-breaking"}
+            />
+        </div>
+    );
+}
+
+function ScholarView(props) {
+    console.log("what is my properties", props);
+    return (
+        <div className="scholar">
+            <div className="scholarName">{props.name}</div>
+            <div className="scholarInterests">{props.interests}</div>
+        </div>
+    );
 }
 
 export default App;
