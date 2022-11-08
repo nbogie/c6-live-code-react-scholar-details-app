c33a0d02f9e3ae9d42e4de4b5babdc2d0ee9e641
nbogie <neillbogie@googlemail.com>
2022-11-08 15:33:34 +0000

    add array of scholar details

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import "./App.css";
 
 function App() {
+
+    const scholarDetails = [
+        {
+            name: "María",
+            interests: "reading classic books, travelling",
+            location: "edinburgh"
+        },
+        {
+            name: 'Zac',
+            interests: "chess codewars",
+            location: "rekjyjyjyavk"
+        },
+        {
+            name: "Maghfoor",
+            interests: "Video Games, Mountain Biking",
+            location: "birmingham"
+        },
+        {
+            name: "Grace",
+            interests: "volleyball, reading",
+            location: "london"
+        },
+        {
+            name: "Ben",
+            interests: "breakdancing, board-breaking",
+            location: "egypt"
+        }
+    ];
+
+
     return (
         <div className="App">
             <h1>Hello Cohort 6</h1>
 
-            <ScholarView name={"Aneesah"} interests={"yoga, watercolor"} />
+            <ScholarView name={"Aneesah"} interests={"yoga, watercolor"} location={"paris"} />
 
-            <ScholarView name={"Alice"} interests={"video games, music"} />
+            <ScholarView name={"Alice"} interests={"video games, music"} location={"rome"} />
 
             <ScholarView
                 name={"Ben"}
                 interests={"breakdancing, board-breaking"}
+                location={"egypt"}
             />
         </div>
     );
 }
 
 function ScholarView(props) {
-    console.log("what is my properties", props);
+
+    // console.log("what is my properties", props);
     return (
         <div className="scholar">
             <div className="scholarName">{props.name}</div>
